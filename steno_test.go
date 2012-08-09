baf98455ba717b4c772416034bc15ccd158ced99
Jeff Li <jeffli@rbcon.com>
2012-08-09 17:26:35 +0800

    Add unit test cases
    Change-Id: I5e29882d720c99c4353f92adac5894ac5a47ae52

---
+++ steno_test.go
@@ -0,0 +1,24 @@
+package steno
+
+import (
+	"testing"
+	"os"
+)
+
+func init() {
+	jsonCodec := NewJsonCodec()
+	config := NewConfig([]Sink{NewIOSink(os.Stdout)}, LOG_INFO.name, jsonCodec)
+	Init(config)
+}
+
+func TestInit(t *testing.T) {
+  defer teardown()
+
+  if &config == nil {
+    t.Error("The config should not be nil")
+  }
+
+	if len(loggers) != 0 {
+		t.Error("The number of loggers at first should be ZERO.")
+	}
+}
