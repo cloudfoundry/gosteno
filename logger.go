a6775c6bbdf10aa3c864c9f8f7e005138e75e542
Anfernee Gui <agui@vmware.com>
2012-08-09 17:41:31 +0800

    Refine log level. Add missing log level methods.
    Change-Id: Id5349f4bf4d312532e55b1458d24842e26ae849e

--- logger.go
+++ logger.go
@@ -14,46 +14,57 @@ func NewLogger(name string) *Logger {
 
 		logger.name = name
 		logger.sinks = config.sinks
-
-		level := lookupLevel(config.level)
-		logger.level = level
+		logger.level = config.level
 
 		loggers[name] = logger
 	}
 
-
 	return logger
 }
 
-func (l *Logger) Log(level string, message string) {
+func (l *Logger) Log(level *LogLevel, message string) {
 	if !l.active(level) {
 		return
 	}
 
 	for _, sink := range l.sinks {
-		record := NewRecord(lookupLevel(level), message)
+		record := NewRecord(level, message)
 
 		sink.AddRecord(record)
 		sink.Flush()
 	}
 }
 
-// TODO: more functions needed
+func (l *Logger) Fatal(message string) {
+	l.Log(LOG_FATAL, message)
+}
+
+func (l *Logger) Error(message string) {
+	l.Log(LOG_ERROR, message)
+}
+
+func (l *Logger) Warn(message string) {
+	l.Log(LOG_WARN, message)
+}
+
 func (l *Logger) Info(message string) {
-	l.Log("info", message)
+	l.Log(LOG_INFO, message)
 }
 
 func (l *Logger) Debug(message string) {
-	l.Log("debug", message)
+	l.Log(LOG_DEBUG, message)
 }
 
-func (l *Logger) active(level string) bool {
-	theLevel := lookupLevel(level)
-	if theLevel == nil {
-		return false
-	}
+func (l *Logger) Debug1(message string) {
+	l.Log(LOG_DEBUG1, message)
+}
 
-	if l.level.priority >= theLevel.priority {
+func (l *Logger) Debug2(message string) {
+	l.Log(LOG_DEBUG2, message)
+}
+
+func (l *Logger) active(level *LogLevel) bool {
+	if l.level.priority >= level.priority {
 		return true
 	}
 
