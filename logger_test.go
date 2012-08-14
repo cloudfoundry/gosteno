44dec769d15725c156f75d58a47bd9265e781f7b
Jeff Li <jeffli@rbcon.com>
2012-08-14 14:32:03 +0800

    Rewrite the unit test cases with gocheck
    Change-Id: I57ef952ddf57b6f635b295243e94348151ba2817

--- logger_test.go
+++ logger_test.go
@@ -1,8 +1,58 @@
 package steno
 
 import (
-	"testing"
+	. "launchpad.net/gocheck"
+	"os"
 )
 
-func TestLogger(t *testing.T) {
+type LoggerSuite struct {
+}
+
+var _ = Suite(&LoggerSuite{})
+
+func (s *LoggerSuite) SetUpTest(c *C) {
+	sinks := []Sink{NewIOSink(os.Stdout)}
+	cfg := Config{}
+	cfg.Sinks = sinks
+	loggers = make(map[string]Logger)
+	Init(&cfg)
+}
+
+func (s *LoggerSuite) TearDownTest(c *C) {
+	config = Config{}
+	loggers = nil
+}
+
+func (s *LoggerSuite) TestLoggersNum(c *C) {
+	c.Assert(len(loggers), Equals, 0)
+}
+
+func (s *LoggerSuite) TestDefaultLevel(c *C) {
+
+}
+
+func (s *LoggerSuite) TestLoggerName(c *C) {
+	logger := NewLogger("test").(*BaseLogger)
+	c.Assert(logger.name, Equals, "test")
+}
+
+func (s *LoggerSuite) TestLoggerLevelActive(c *C) {
+	logger := NewLogger("foo").(*BaseLogger)
+	defaultLevel := LOG_INFO
+	c.Assert(logger.active(defaultLevel), Equals, true)
+}
+
+func (s *LoggerSuite) TestLoggerLevelActive2(c *C) {
+	logger := NewLogger("bar").(*BaseLogger)
+	logger.level = LOG_INFO
+	higherLevels := []*LogLevel{LOG_WARN, LOG_ERROR, LOG_ERROR}
+	for _, level := range higherLevels {
+		c.Assert(logger.active(level), Equals, true)
+	}
+}
+
+func (s *LoggerSuite) TestCreatingDupLogger(c *C) {
+	logger1 := NewLogger("foobar")
+	logger2 := NewLogger("foobar")
+	c.Assert(logger1, Equals, logger2)
 }
