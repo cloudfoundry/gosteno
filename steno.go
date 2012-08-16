6cb8031d9d4b6865d552de51a2f0b0f7c9d3c216
Anfernee Gui <agui@vmware.com>
2012-08-16 16:27:27 +0800

    Enable setting log level by log name matching
    Change-Id: Ica4f057f455dbf729e1791f1b9b5ed4960a9aca2

--- steno.go
+++ steno.go
@@ -2,12 +2,22 @@ package steno
 
 import (
 	"encoding/json"
+	"regexp"
 	"sync"
 )
 
+// Global configs
 var config Config
-var loggers = make(map[string]Logger)
+
+// loggersMutex protects accesses to loggers and regexp
 var loggersMutex sync.Mutex
+
+// loggers only saves BaseLogger
+var loggers = make(map[string]*BaseLogger)
+
+// loggerRegexp* used to match log name and log level
+var loggerRegexp *regexp.Regexp
+var loggerRegexpLevel *LogLevel
 
 func Init(c *Config) {
 	config = *c
@@ -29,6 +39,86 @@ func Init(c *Config) {
 	if config.Port > 0 {
 		initHttpServer(config.Port)
 	}
+}
+
+func NewLogger(name string) Logger {
+	loggersMutex.Lock()
+	defer loggersMutex.Unlock()
+
+	logger := loggers[name]
+
+	if logger == nil {
+		baseLogger := new(BaseLogger)
+
+		baseLogger.name = name
+		baseLogger.sinks = config.Sinks
+		baseLogger.level = computeLevel(name)
+
+		logger = baseLogger
+		loggers[name] = logger
+	}
+
+	return logger
+}
+
+func SetLoggerRegexp(pattern string, level *LogLevel) (bool, error) {
+	loggersMutex.Lock()
+	defer loggersMutex.Unlock()
+
+	clearLoggerRegexp()
+	return setLoggerRegexp(pattern, level)
+}
+
+func ClearLoggerRegexp() {
+	loggersMutex.Lock()
+	defer loggersMutex.Unlock()
+
+	clearLoggerRegexp()
+}
+
+//
+// Private functions
+//
+
+func setLoggerRegexp(pattern string, level *LogLevel) (bool, error) {
+	regExp, err := regexp.Compile(pattern)
+	if err != nil {
+		return false, err
+	}
+
+	// If here, Logger regexp is valid
+	loggerRegexp = regExp
+	loggerRegexpLevel = level
+	for name, logger := range loggers {
+		if loggerRegexp.MatchString(name) {
+			logger.level = loggerRegexpLevel
+		}
+	}
+
+	return true, nil
+}
+
+func clearLoggerRegexp() {
+	if loggerRegexp == nil {
+		return
+	}
+
+	for name, logger := range loggers {
+		if loggerRegexp.MatchString(name) {
+			logger.level = config.Level
+		}
+	}
+
+	loggerRegexp = nil
+	loggerRegexpLevel = nil
+}
+
+func computeLevel(name string) *LogLevel {
+	if loggerRegexp != nil && loggerRegexp.MatchString(name) {
+		return loggerRegexpLevel
+	}
+
+	return config.Level
 }
 
 func loggersInJson() string {
