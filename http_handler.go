55e6960855de562bbc6557bd84ab0d9967d687df
Jeff Li <jeffli@rbcon.com>
2012-08-27 16:57:22 +0800

    Change the level through http interface
    At the moment 5 request methods are implemented, all data are exchanged
    as json.
    1. GET /regexp : get something like {"RexExp": "test$", "Level": "fatal"}
    2. PUT /regexp : put with data like {"RegExp": "test$", "Level":"fatal"}
    3. GET /loggers/{name} : get information about the logger by name
    4. PUT /loggers/{name} : put with data like {"Level" : "fatal" }
    5. GET /loggers : get information about all loggers
    
    Change-Id: Ia39f22ab0e533df3e4c6059fa17b9b784b4efbfe

--- http_handler.go
+++ http_handler.go
@@ -1,24 +1,120 @@
 package steno
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if strings.EqualFold(r.Method, "GET") {
-		io.WriteString(w, loggersInJson())
-	} else {
-		// TODO: PUT not implemented
+const (
+	HTTP_ROOT_PATH         = "/"
+	HTTP_REGEXP_PATH       = "/regexp"
+	HTTP_LIST_LOGGERS_PATH = "/loggers"
+	HTTP_LOGGER_PATH       = "/logger/"
+)
+
+func loggerHandler(w http.ResponseWriter, r *http.Request) {
+	loggerName := r.URL.Path[len(HTTP_LOGGER_PATH):]
+	logger, ok := loggers[loggerName]
+	if !ok {
+		http.Error(w, "No logger with the name exists : "+loggerName, http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		bytes, _ := logger.MarshalJSON()
+		if _, err := w.Write(bytes); err != nil {
+			log.Println(err)
+		}
+
+	case "PUT":
+		var levelParams struct{ Level string }
+		jsonData, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err = json.Unmarshal(jsonData, &levelParams); err != nil {
+			http.Error(w, "Can't parse the parameters", http.StatusBadRequest)
+			return
+		}
+		level, ok := LEVELS[levelParams.Level]
+		if !ok {
+			http.Error(w, "No level with that name exists : "+levelParams.Level, http.StatusBadRequest)
+			return
+		}
+		logger.level = level
+
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func regExpHandler(w http.ResponseWriter, r *http.Request) {
+	type regexpParams struct {
+		RegExp string
+		Level  string
+	}
+	switch r.Method {
+	case "GET":
+		bytes, err := json.Marshal(regexpParams{loggerRegexp.String(), loggerRegexpLevel.name})
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		if _, err = w.Write(bytes); err != nil {
+			log.Println(err)
+		}
+
+	case "PUT":
+		jsonData, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		params := regexpParams{}
+		err = json.Unmarshal(jsonData, &params)
+		if err != nil {
+			http.Error(w, "Can't parse parameters", http.StatusBadRequest)
+			return
+		}
+		level, ok := LEVELS[params.Level]
+		if !ok {
+			http.Error(w, "No level with the name exists : "+params.Level, http.StatusBadRequest)
+			return
+		}
+		err = SetLoggerRegexp(params.RegExp, level)
+		if err != nil {
+			http.Error(w, "The parameter of regexp is not correct", http.StatusBadRequest)
+			return
+		}
+
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func loggersListHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		if _, err := w.Write([]byte(loggersInJson())); err != nil {
+			log.Println(err)
+		}
+	default:
 		http.NotFound(w, r)
 	}
 }
 
 func initHttpServer(port int) {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/loggers", handler)
+
+	mux.HandleFunc(HTTP_REGEXP_PATH, regExpHandler)
+	mux.HandleFunc(HTTP_LOGGER_PATH, loggerHandler)
+	mux.HandleFunc(HTTP_LIST_LOGGERS_PATH, loggersListHandler)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
