8ede77fa75105bf20b79513217803881dfbf32c6
Jeff Li <jeffli@rbcon.com>
2012-08-27 16:57:22 +0800

    Change the level through http interface
    The request to change level is a 'PUT' or 'POST' method with two parameters
    'regexp' and 'level'.The http server will get these two parameters and
    invoke the 'SetLoggerRegExp' method to change the level on the fly.
    
    Change-Id: Ia39f22ab0e533df3e4c6059fa17b9b784b4efbfe

---
+++ html_templates.go
@@ -0,0 +1,55 @@
+package steno
+
+//TODO: This templates should be shipped as standalone template
+//      files. However, it seems that go doesn't support package
+//      resource fils as binary library directly. Figure out how
+//      to distribte package with assets file such as *.html, *.css
+
+const index_page_template = `
+<!doctype html>
+
+<html>
+	<body>
+    <script>
+      function sendPutRequest(form, path) {
+				var data = {};
+				data.regexp = form.regexp.value;
+				data.level =  form.level.value;
+				var dataJson = JSON.stringify(data)
+				url = document.URL.concat(path)
+        xmlHttp = new XMLHttpRequest();
+				xmlHttp.open('PUT', url, true);
+				xmlHttp.setRequestHeader('Content-type','application/json; charset=utf-8');
+				xmlHttp.onreadystatechange = function() {
+					document.location.reload(true)
+				}
+        xmlHttp.send(dataJson);
+				return false;
+      }
+    </script>
+		<fieldset>
+			<legend>Information about all loggers</legend>
+			<dl>
+				{{range $k, $v := .LoggersInfo}}
+				<dt>{{$k}}</dt>
+				<dd>- {{$v}}</dd>
+				{{end}}
+			</dl>
+		</fieldset>
+
+		<form onsubmit="return sendPutRequest(this, 'loggers')" name="change_level">
+			<fieldset>
+				<legend>Change level here</legend>
+				regexp: <input type="text" name="regexp"/>
+				level:
+				<select name="level">
+					{{range .Levels}}
+					<option value="{{.}}">{{.}}</option>
+					{{end}}
+				</select></br>
+				<input type="submit" value="Change" >
+			</fieldset>
+		</form>
+	</body>
+</html>
+`
--- http_handler.go
+++ http_handler.go
@@ -1,17 +1,88 @@
 package steno
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"sort"
 	"strings"
+	"text/template"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if strings.EqualFold(r.Method, "GET") {
+	if r.URL.Path != "/loggers" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	switch r.Method {
+	case "GET":
 		io.WriteString(w, loggersInJson())
+
+	case "PUT":
+		jsonData, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var message struct {
+			Regexp string
+			Level  string
+		}
+		err = json.Unmarshal(jsonData, &message)
+		if err != nil {
+			panic(err)
+		}
+		level, ok := LEVELS[message.Level]
+		if !ok {
+			http.Error(w, "The parameter of level is not correct", http.StatusBadRequest)
+			return
+		}
+		_, err = SetLoggerRegexp(message.Regexp, level)
+		if err != nil {
+			http.Error(w, "The parameter of regexp is not correct", http.StatusBadRequest)
+			return
+		}
+
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	if strings.EqualFold(r.Method, "GET") {
+		page, err := template.New("index page").Parse(index_page_template)
+		if err != nil {
+			panic(err)
+		}
+
+		loggersInfo := make(map[string]string)
+		for k, v := range loggers {
+			bytes, _ := v.MarshalJSON()
+			loggersInfo[k] = string(bytes)
+		}
+
+		i := 0
+		levels := make([]string, len(LEVELS))
+		for k, _ := range LEVELS {
+			levels[i] = k
+			i++
+		}
+		sort.Strings(levels)
+
+		page.Execute(w, struct {
+			LoggersInfo map[string]string
+			Levels      []string
+		}{loggersInfo, levels})
 	} else {
-		// TODO: PUT not implemented
 		http.NotFound(w, r)
 	}
 }
@@ -19,6 +90,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func initHttpServer(port int) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/loggers", handler)
+	mux.HandleFunc("/", index)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
