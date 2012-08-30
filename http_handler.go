ead29866c94e65242b073288b6c8b4af1ed59699
Anfernee Gui <agui@vmware.com>
2012-08-30 18:35:09 +0800

    Support tailing logs through http websocket
    Change-Id: I3ddf65b2be4cfe092fa03d054b40628735948fdb

--- http_handler.go
+++ http_handler.go
@@ -1,11 +1,18 @@
 package steno
 
 import (
+	"code.google.com/p/go.net/websocket"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+type chanSet map[chan []byte]bool
+
+var wsChans = make(map[string]chanSet)
+var wsMutex sync.RWMutex
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if strings.EqualFold(r.Method, "GET") {
@@ -16,9 +23,48 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func tailServer(rw *websocket.Conn) {
+	path := rw.Request().RequestURI
+	logName := strings.Split(path, "/")[2]
+
+	if loggers[logName] == nil {
+		// TODO: How to gracefully close ws conn
+		rw.Write([]byte("logger not found"))
+		rw.Close()
+		return
+	}
+
+	wsMutex.Lock()
+
+	if wsChans[logName] == nil {
+		wsChans[logName] = make(chanSet)
+	}
+	set := wsChans[logName]
+
+	ch := make(chan []byte)
+	set[ch] = true
+
+	wsMutex.Unlock()
+
+	for {
+		msg := <-ch
+		n, err := rw.Write(msg)
+
+		// TODO: Need to confirm: this === remote conn closed
+		if n == 0 || err != nil {
+			break
+		}
+	}
+
+	wsMutex.Lock()
+	delete(set, ch)
+	wsMutex.Unlock()
+}
+
 func initHttpServer(port int) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/loggers", handler)
+	mux.Handle("/tail/", websocket.Handler(tailServer))
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
