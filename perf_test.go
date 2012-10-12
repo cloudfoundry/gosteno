0c54ed709c765e7e3dc740e0fb748b0519292445
Jeff Li <jeffli@rbcon.com>
2012-10-12 10:33:16 +0800

    fix the problem when running benchmark cases
    global variable 'loggers' need to be initialized
    
    Change-Id: I4df74c5fd9493d3296ce119fd1e33aef5527ead1

--- perf_test.go
+++ perf_test.go
@@ -5,41 +5,52 @@ import (
 )
 
 func BenchmarkNoSink(b *testing.B) {
+	b.StopTimer()
 	Init(&Config{})
+	loggers = make(map[string]*BaseLogger)
 	logger := NewLogger("nosink")
+	b.StartTimer()
 
 	performBenchmark(logger, b.N)
 }
 
 func BenchmarkDevNullSink(b *testing.B) {
+	b.StopTimer()
 	Init(&Config{
 		Sinks: []Sink{NewFileSink("/dev/null")},
 	})
+	loggers = make(map[string]*BaseLogger)
 	logger := NewLogger("dev_null_sink")
+	b.StartTimer()
 
 	performBenchmark(logger, b.N)
 }
 
 func BenchmarkDevNullSinkWithLOC(b *testing.B) {
+	b.StopTimer()
 	Init(&Config{
 		Sinks:     []Sink{NewFileSink("/dev/null")},
 		EnableLOC: true,
 	})
+	loggers = make(map[string]*BaseLogger)
 	logger := NewLogger("dev_null_sink_with_loc")
+	b.StartTimer()
 
 	performBenchmark(logger, b.N)
 }
 
 func BenchmarkTaggedLoggerInDevNullSink(b *testing.B) {
+	b.StopTimer()
 	Init(&Config{
 		Sinks: []Sink{NewFileSink("/dev/null")},
 	})
-
+	loggers = make(map[string]*BaseLogger)
 	tags := map[string]string{
 		"thread_id":    "1234",
 		"program_name": "benchmark",
 	}
 	logger := NewTaggedLogger(NewLogger("dev_null_sink_tagged"), tags)
+	b.StartTimer()
 
 	performBenchmark(logger, b.N)
 }
